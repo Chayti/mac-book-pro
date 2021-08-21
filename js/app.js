8cea58e07b84953ec15c840066e50ada1ffa9a5a
Chayti Saha <chaytisaha98@gmail.com>
2021-08-21 09:41:31 +0600

    got DOM elements

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// memory type buttons
+const memoryType1Btn = document.getElementById('memory-type1-btn');
+const memoryType2Btn = document.getElementById('memory-type2-btn');
+
+// storage type buttons
+const storageType1Btn = document.getElementById('storage-type1-btn');
+const storageType2Btn = document.getElementById('storage-type2-btn');
+const storageType3Btn = document.getElementById('storage-type3-btn');
+
+// delivery type buttons
+const deliveryType1Btn = document.getElementById('delivery-type1-btn');
+const deliveryType2Btn = document.getElementById('delivery-type2-btn');
+
+// costing
+const bestPrice = document.getElementById('best-price'); //best price
+const extraMemoryCost = document.getElementById('extra-memory-cost'); //extra memory cost
+const extraStorageCost = document.getElementById('extra-storage-cost'); //extra storage cost
+const deliveryCharge = document.getElementById('delivery-charge'); //delivery charge
+const total = document.getElementById('total'); //total
+
+// promo code
+const promoCode = document.getElementById('promo-code');
+const promoCodeBtn = document.getElementById('promo-code-btn');
+
+// total with promo code
+const totalWithPromoCode = document.getElementById('total-w-promo');
