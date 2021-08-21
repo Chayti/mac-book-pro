2c677b1a54d27f892ebb17fe3521d83003876ad5
Chayti Saha <chaytisaha98@gmail.com>
2021-08-21 11:07:11 +0600

    js functionlity added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+/* =============================
+    getting DOM objects
+=============================== */
+
 // memory type buttons
 const memoryType1Btn = document.getElementById('memory-type1-btn');
 const memoryType2Btn = document.getElementById('memory-type2-btn');
@@ -16,11 +20,82 @@ const bestPrice = document.getElementById('best-price'); //best price
 const extraMemoryCost = document.getElementById('extra-memory-cost'); //extra memory cost
 const extraStorageCost = document.getElementById('extra-storage-cost'); //extra storage cost
 const deliveryCharge = document.getElementById('delivery-charge'); //delivery charge
-const total = document.getElementById('total'); //total
+const totalPrice = document.getElementById('total'); //total
 
 // promo code
 const promoCode = document.getElementById('promo-code');
 const promoCodeBtn = document.getElementById('promo-code-btn');
 
 // total with promo code
-const totalWithPromoCode = document.getElementById('total-w-promo');
+const totalPriceWithPromoCode = document.getElementById('total-w-promo');
+
+
+/* =============================
+    functions & event handlers
+=============================== */
+
+// calculate total price without promo code
+function getTotalPrice() {
+    totalPrice.innerText = parseInt(bestPrice.innerText) + parseInt(extraMemoryCost.innerText) + parseInt(extraStorageCost.innerText) + parseInt(deliveryCharge.innerText);
+
+    totalPriceWithPromoCode.innerText = totalPrice.innerText;
+}
+
+// // calculate total price with promo code
+// function getTotalPriceWithPromoCode() {
+// }
+
+// extra memory cost
+function getExtraMemoryCost(btn) { //adding extra memory cost
+    if (btn == 'btn1') extraMemoryCost.innerText = 0;
+    else extraMemoryCost.innerText = 180;
+}
+memoryType1Btn.addEventListener('click', function () { //extra memory cost event handler
+    getExtraMemoryCost('btn1');
+    getTotalPrice();
+})
+memoryType2Btn.addEventListener('click', function () { //extra memory cost event handler
+    getExtraMemoryCost('btn2');
+    getTotalPrice();
+})
+
+// extra storage cost
+function getExtraStorageCost(btn) { //adding extra storage cost
+    if (btn == 'btn1') extraStorageCost.innerText = 0;
+    else if (btn == 'btn2') extraStorageCost.innerText = 100;
+    else extraStorageCost.innerText = 180;
+}
+storageType1Btn.addEventListener('click', function () { //extra storage cost event handler
+    getExtraStorageCost('btn1');
+    getTotalPrice();
+})
+storageType2Btn.addEventListener('click', function () { //extra storage cost event handler
+    getExtraStorageCost('btn2');
+    getTotalPrice();
+})
+storageType3Btn.addEventListener('click', function () { //extra storage cost event handler
+    getExtraStorageCost('btn3');
+    getTotalPrice();
+})
+
+// delivery charge
+function getDeliveryCharge(btn) { //adding delivery charge
+    if (btn == 'btn1') deliveryCharge.innerText = 0;
+    else deliveryCharge.innerText = 20;
+}
+deliveryType1Btn.addEventListener('click', function () { //delivery charge event handler
+    getDeliveryCharge('btn1');
+    getTotalPrice();
+})
+deliveryType2Btn.addEventListener('click', function () { //delivery charge event handler
+    getDeliveryCharge('btn2');
+    getTotalPrice();
+})
+
+// promo code
+promoCodeBtn.addEventListener('click', function () {
+    if (promoCode.value == 'stevekaku')
+        totalPriceWithPromoCode.innerText = parseInt(totalPrice.innerText) - parseInt(totalPrice.innerText) * 0.2;
+    else
+        totalPriceWithPromoCode.innerText = parseInt(totalPrice.innerText);
+})
